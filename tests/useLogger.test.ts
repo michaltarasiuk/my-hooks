408f28465131fce6aa8f8fe72f174e57814e5223
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-02-09 17:39:12 +0100

    update useLogger hook test

--- tests/useLogger.test.ts
+++ tests/useLogger.test.ts
@@ -4,38 +4,33 @@ import { useLogger } from '../src/useLogger'
 const logSpy = jest.spyOn(global.console, 'log').mockImplementation(() => {})
 
 describe('useLogger', () => {
-  it('should be defined', () => {
-    expect(useLogger).toBeDefined()
-  })
-
   it('should log the provided props on mount', () => {
-    const props = { question: 'What is the meaning?', answer: 42 }
-    renderHook(() => useLogger('Test', props))
+    // arrange
+    renderHook(() => useLogger('component'))
 
+    // assert
     expect(logSpy).toBeCalledTimes(2)
   })
 
   it('should log when the component has unmounted', () => {
-    const props = { question: 'What is the meaning?', answer: 42 }
-    const { unmount } = renderHook(() => useLogger('Test', props))
+    // given
+    const { unmount } = renderHook(() => useLogger('component'))
 
+    // when
     unmount()
 
+    // then
     expect(logSpy).toBeCalledTimes(3)
   })
 
   it('should log updates as props change', () => {
-    const { rerender } = renderHook(
-      ({ componentName, props }: { componentName: string; props: any }) =>
-        useLogger(componentName, props),
-      {
-        initialProps: { componentName: 'Test', props: { one: 1 } },
-      }
-    )
+    // given
+    const { rerender } = renderHook(() => useLogger('component'))
 
-    const newProps = { one: 1, two: 2 }
-    rerender({ componentName: 'Test', props: newProps })
+    // when
+    rerender()
+    rerender()
 
-    expect(logSpy).toHaveBeenCalledTimes(3)
+    expect(logSpy).toHaveBeenCalledTimes(4)
   })
 })
