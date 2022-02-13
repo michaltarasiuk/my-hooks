e5981a68277ea5da38f4b1561be39c04b11ac8d1
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-02-13 22:06:29 +0100

    create delayed state hook

---
+++ src/useDelayedState.ts
@@ -0,0 +1,34 @@
+import { useState, useCallback } from 'react'
+
+import { useDidUpdate } from './useDidUpdate'
+
+export const useDelayedState = <TValue>(
+  initialValue: TValue,
+  condition: any
+) => {
+  const [state, setState] = useState({
+    value: initialValue,
+    loaded: false,
+  })
+
+  useDidUpdate(
+    () => {
+      if (!state.loaded && condition) {
+        setState((prevState) => ({ ...prevState, loaded: true }))
+      }
+    },
+    condition,
+    state.loaded
+  )
+
+  const updateState = useCallback(
+    (newValue: TValue) => {
+      if (state.loaded) {
+        setState((prevState) => ({ ...prevState, value: newValue }))
+      }
+    },
+    [state]
+  )
+
+  return [state, updateState] as const
+}
